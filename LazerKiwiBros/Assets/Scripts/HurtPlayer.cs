ebb54076a01725822e03d3399c33a2070e5a215f
Jet Yong <jettlag2000@gmail.com>
2020-08-13 15:15:24 +1200

    Bullet Controller, Enemy Controller and Health

--- LazerKiwiBros/Assets/Scripts/HurtPlayer.cs
+++ LazerKiwiBros/Assets/Scripts/HurtPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class newscript : MonoBehaviour
+public class HurtPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
