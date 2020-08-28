a6beed747e87b3424705cc6b464cdb9d21d82537
Jet Yong <cherryyong4075@gmail.com>
2020-08-28 19:28:59 +1200

    WorkingLaserWIP#1

---
+++ LazerKiwiBros/Assets/Scripts/Laser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public bool shootLaser = false;
+    public LineRenderer lineRenderer;
+
+    void LaserFiring()
+    {
+        if (shootLaser == true)
+        {
+            
+        }
+    }
+}
